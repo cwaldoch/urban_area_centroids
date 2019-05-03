0cf71b14e629f4706f37a67445660b0a7da976d6
Connor Waldoch <cwal37@gmail.com>
2019-05-03 11:03:19 -0400

    Update centroid_UA_map.py

--- centroid_UA_map.py
+++ centroid_UA_map.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri May  3 10:33:54 2019
-
-@author: CWaldoch
-"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,10 +12,10 @@ mpl.rcParams['font.size'] = 16
 
 df = pd.read_csv(r"C:\Users\cwaldoch\Desktop\urban_area_centroids.csv")
 
-df['size'] = df['POP']**0.35
-
+#df['size'] = df['POP']**0.45
+df['size'] = [1 if x <20000 else x**0.45 for x in df['POP'].values]
 plt.scatter(df['x'], df['y'], s=df['size'], alpha=0.5)
 plt.xlim()
 plt.axis('off')
 plt.tight_layout()
-plt.savefig('UA_centroid.png', dpi=300)
+plt.savefig('UA_centroid_dual_rule.png', dpi=300)
